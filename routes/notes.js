ad6690e5527dc983276ff968673389f935ca3c1e
Ben Binder <ben.binder11@gmail.com>
2022-11-12 13:44:39 -0600

    Replace homemade uuid with npm package.

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,9 @@
 const notes = require('express').Router();
 const fs = require('fs');
-const uuid = require('../helpers/uuid');
+const { v4: uuidv4 } = require('uuid') ;
 
-notes.get('/', async (req, res) => {
+notes.get('/', (req, res) => {
+  // Read the "database" file and return the JSON array inside.
   fs.readFile('./db/db.json', {encoding: 'utf8'}, (err,db) => {
     res.json(JSON.parse(db));
   });
@@ -10,17 +11,20 @@ notes.get('/', async (req, res) => {
 
 notes.post('/', (req, res) => {
   // Generate a new id for the incoming note.
-  const new_id = uuid();
+  const new_id = uuidv4();
   // Read the JSON "database" file and parse it.
   fs.readFile('./db/db.json', {encoding: 'utf8'}, (err,db) => {
     const db_json = JSON.parse(db);
     const { title, text } = req.body;
+    // Build a note object containing the new id.
     const new_note = {
       id : new_id,
       title : title,
       text : text
     }
+    // Append the new note the end of the database JSON array.
     db_json.push(new_note);
+    // Write the modified array to the "database" file.
     fs.writeFile('./db/db.json',JSON.stringify(db_json), (err) => {
       res.send(err ? err : 'save success');
     });
